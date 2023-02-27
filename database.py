f3baf6d9cf1d4e2cb07eb703a2d6541f2e69b211
Paola <115275132+rosepad21@users.noreply.github.com>
2023-02-27 17:23:28 +0000

    created dynamic pages for each job

--- database.py
+++ database.py
@@ -10,11 +10,25 @@ engine = create_engine(os.environ['DB_CONNECTION_STRING'],
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text('select * from jobs'))
+    result = conn.execute(text('SELECT * FROM jobs'))
     jobs = []
     for row in result.all():
       jobs.append(row._mapping)
       # this is how we convert it to a dict
       # note that dict(row) doesn't work in > 1.4 vers of sqlalchemy
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id={id}")
+      )
+    rows = []
+    for row in result.all():
+      rows.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return [dict(row) for row in rows]
+    
     
