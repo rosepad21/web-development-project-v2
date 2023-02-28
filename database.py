f083300fc77a8ff9990053ccc3af52e3c690fb13
Paola <115275132+rosepad21@users.noreply.github.com>
2023-02-28 16:29:05 +0000

    added application form

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, exc
 
 engine = create_engine(os.environ['DB_CONNECTION_STRING'],
   connect_args={
@@ -30,5 +30,14 @@ def load_job_from_db(id):
       return None
     else:
       return [dict(row) for row in rows]
-    
+
+def add_application_to_db(id, data):
+  with engine.connect() as conn:
+    query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({id}, {data['full_name']}, {data['email']}, {data['linkedin_url']}, {data['education']}, {data['work_experience']}, {data['resume_url']})")
+    try:
+      conn.execute(query)
+    except exc.DBAPIError as e:
+    # an exception is raised, Connection is invalidated.
+      if e.connection_invalidated:
+          print("Connection was invalidated!")
     
